de2d29a4f769eca510eca613cfdb966fccfdee43
0x410x <alex.harner@gmail.com>
2024-11-30 20:16:21 -0700

    deployment

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling when clicking on a navigation link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
